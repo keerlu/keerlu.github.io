2e3985d1ab7a125ad9b7a4ddb3c213fe298ac0cc
Unknown <lucy.keer@gmail.com>
2017-06-05 07:03:01 +0100

    Kaleidoscope html and js files

---
+++ kaleidoscope/kaleidoscope.js
@@ -0,0 +1,9 @@
+var circle = d3.selectAll("circle");
+
+// circle.style("fill", "steelblue");
+
+circle.attr("cx", function() { return Math.random() * 800; });
+
+// next task: add new circles to the svg container.
+
+
